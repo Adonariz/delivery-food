9d660ae0e1aa575dac04cb95f488ffe8a573b668
A Zhila <aleksej.zhila@gmail.com>
2020-05-05 16:07:49 +0300

    Добавляет авторизацию пользователя и проверку логина

--- js/main.js
+++ js/main.js
@@ -8,3 +8,92 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// day 1
+
+const authButton = document.querySelector(`.button-auth`);
+const modalAuth = document.querySelector(`.modal-auth`);
+const authModalCloseButton = modalAuth.querySelector(`.close-auth`);
+
+const logInForm = modalAuth.querySelector(`#logInForm`);
+const loginInput = logInForm.querySelector(`#login`);
+
+const userName = document.querySelector(`.user-name`);
+const buttonOut = document.querySelector(`.button-out`);
+
+let login = localStorage.getItem('login');
+
+const onAuthButtonClick = () => {
+  modalAuth.classList.add(`is-open`);
+  authModalCloseButton.addEventListener(`click`, onAuthModalCloseButtonClick);
+  document.addEventListener(`keydown`, onDocumentEscKeydown);
+}
+
+const onAuthModalCloseButtonClick = () => {
+  modalAuth.classList.remove(`is-open`);
+  authModalCloseButton.removeEventListener(`click`, onAuthModalCloseButtonClick);
+}
+
+const onDocumentEscKeydown = (evt) => {
+  const ESC_KEY = `Escape`;
+
+  if (evt.key === ESC_KEY) {
+    evt.preventDefault();
+    modalAuth.classList.remove(`is-open`);
+    document.removeEventListener(`keydown`, onDocumentEscKeydown);
+  }
+};
+
+const onLogInFormSubmit = (evt) => {
+  evt.preventDefault();
+  login = loginInput.value;
+
+  localStorage.setItem(`login`, login);
+
+  if (!loginInput.value) {
+    alert(`Введите логин`);
+  } else {
+    modalAuth.classList.remove(`is-open`);
+    logInForm.reset();
+    checkAuth();
+  }
+};
+
+const onButtonOutClick = () => {
+  login = ``;
+  userName.textContent = ``;
+
+  userName.style.display = ``;
+  authButton.style.display = ``;
+  buttonOut.style.display = ``;
+
+  localStorage.removeItem(`login`);
+  buttonOut.removeEventListener(`click`, onButtonOutClick);
+
+  checkAuth();
+};
+
+const authorised = () => {
+  authButton.style.display = `none`;
+  buttonOut.style.display = `block`;
+
+  userName.style.display = `inline`;
+  userName.textContent = `Привет, ${login}`;
+
+  buttonOut.addEventListener(`click`, onButtonOutClick);
+};
+
+const notAuthorised = () => {
+  authButton.addEventListener(`click`, onAuthButtonClick);
+  logInForm.addEventListener(`submit`, onLogInFormSubmit);
+};
+
+const checkAuth = () => {
+  if (login) {
+    authorised();
+  } else {
+    notAuthorised();
+  }
+};
+
+checkAuth();
